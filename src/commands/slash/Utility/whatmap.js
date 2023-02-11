7e6786748a10f23ac8cb9c46f8b6285172b4d885
Christopher Young <chrisyoungplus@outlook.com>
2023-02-11 05:21:54 +0000

    Whatmap Command Fixs #2

--- src/commands/slash/Utility/whatmap.js
+++ src/commands/slash/Utility/whatmap.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
+const { constants } = require('buffer');
 
 const filePath = './src/data/maps.txt';
 
@@ -50,28 +51,35 @@ module.exports = {
         if (!commandInt==null || !commandInt=="") {
 
             if(commandInt === 'removemap' || commandInt === 'rm') {
+                const mapInt = mapIntUpper.toLowerCase();
                 const mapToRemove = mapInt;
 
                 const index = maps.indexOf(mapToRemove);
-                if (index === -1) {
-                    return interaction.reply(`Map ${mapToRemove} not found`);
+                if (!index === -1) {
+                    return interaction.reply({content : `Map ${mapToRemove} not found`, ephemeral: true });
                 }
 
+                
+
+                console.log(mapToRemove)
                 // Remove the map from the array
                 maps.splice(index, 1);
 
-                fs.writeFile(filePath, mapInt + ('\n'), (err, data) => {
+                console.log(maps)
+
+                fs.writeFile(filePath, maps + ('\n'), (err, data) => {
                     if (err) {
                         console.error(err);
                         return;
                     } 
-                    interaction.reply(`Removed ${mapToRemove} from the map list`);
+                    interaction.reply({content : `Removed ${mapToRemove} from the map list`, ephemeral: true });
                 });
             } else if (commandInt === 'addmap' || commandInt === 'am'|| commandInt === 'ad' || commandInt === 'add') {
                 const mapInt = mapIntUpper.toLowerCase();
                 if (maps.length >= maxMaps) {
                     return interaction.reply(`Can only have ${maxMaps} maps stored at a time`);
                 } else if (!mapInt==null || !mapInt==""){
+                    const isMatch = maps.includes(mapInt);
                     const mapsToAdd0 = mapInt.split(' ').slice(0, 1);
                     const mapsToAdd1 = mapInt.split(' ').slice(1, 2);
                     const mapsToAdd2 = mapInt.split(' ').slice(2, 3);
@@ -83,100 +91,102 @@ module.exports = {
 
                     console.log(mapInt)
 
-                    if (mapsToAdd0 == 'shoreline' || mapsToAdd0 == 'customs' || mapsToAdd0== 'reserve' || mapsToAdd0== 'lighthouse' || mapsToAdd0== 'streets' || mapsToAdd0== 'interchange' || mapsToAdd0=='woods' || mapsToAdd0=='factory') {
-                        interaction.reply({content : `Adding ${mapInt} to the map list`, ephemeral: true });
-
-                        console.log(mapsToAdd0)
-                        maps.push(mapsToAdd0);
-                        
-                        fs.appendFile(filePath, mapsToAdd0 + '\n', (err, data) => {
-                            if (err) {
-                                console.error(err);
-                            return;
-                            }
-                        });
-                        if (!mapsToAdd1.length == 0){
-                            console.log(mapsToAdd1)
-                            maps.push(mapsToAdd1);
-                            fs.appendFile(filePath, mapsToAdd1 + '\n', (err, data) => {
+                    if(!isMatch) {
+                        if (mapsToAdd0 == 'shoreline' || mapsToAdd0 == 'customs' || mapsToAdd0== 'reserve' || mapsToAdd0== 'lighthouse' || mapsToAdd0== 'streets' || mapsToAdd0== 'interchange' || mapsToAdd0=='woods' || mapsToAdd0=='factory') {
+                            interaction.reply({content : `Adding ${mapInt} to the map list`, ephemeral: true });
+    
+                            console.log(mapsToAdd0)
+                            maps.push(mapsToAdd0);
+                            
+                            fs.appendFile(filePath, mapsToAdd0 + '\n', (err, data) => {
                                 if (err) {
                                     console.error(err);
                                 return;
-                            }
-                        });
-                            if (!mapsToAdd2.length == 0){
-                                console.log(mapsToAdd2)
-                                maps.push(mapsToAdd2);
-                                
-                                fs.appendFile(filePath, mapsToAdd2 + '\n', (err, data) => {
+                                }
+                            });
+                            if (!mapsToAdd1.length == 0){
+                                console.log(mapsToAdd1)
+                                maps.push(mapsToAdd1);
+                                fs.appendFile(filePath, mapsToAdd1 + '\n', (err, data) => {
                                     if (err) {
                                         console.error(err);
                                     return;
-                                } 
+                                }
                             });
-                                if (!mapsToAdd3.length == 0){
-                                    console.log(mapsToAdd3)
-                                    maps.push(mapsToAdd3);
+                                if (!mapsToAdd2.length == 0){
+                                    console.log(mapsToAdd2)
+                                    maps.push(mapsToAdd2);
                                     
-                                    fs.appendFile(filePath, mapsToAdd3 + '\n', (err, data) => {
+                                    fs.appendFile(filePath, mapsToAdd2 + '\n', (err, data) => {
                                         if (err) {
                                             console.error(err);
                                         return;
-                                    }  
+                                    } 
                                 });
-                                    if (!mapsToAdd4.length == 0){
-                                        console.log(mapsToAdd4)
-                                        maps.push(mapsToAdd4);
+                                    if (!mapsToAdd3.length == 0){
+                                        console.log(mapsToAdd3)
+                                        maps.push(mapsToAdd3);
                                         
-                                        fs.appendFile(filePath, mapsToAdd4 + '\n', (err, data) => {
+                                        fs.appendFile(filePath, mapsToAdd3 + '\n', (err, data) => {
                                             if (err) {
                                                 console.error(err);
                                             return;
-                                        } 
-                                        //interaction.reply(`Added ${mapsToAdd4} to the map list`);
+                                        }  
                                     });
-                                        if (!mapsToAdd5.length == 0){
-                                            console.log(mapsToAdd5)
-                                            maps.push(mapsToAdd5);
+                                        if (!mapsToAdd4.length == 0){
+                                            console.log(mapsToAdd4)
+                                            maps.push(mapsToAdd4);
                                             
-                                            fs.appendFile(filePath, mapsToAdd5 + '\n', (err, data) => {
+                                            fs.appendFile(filePath, mapsToAdd4 + '\n', (err, data) => {
                                                 if (err) {
                                                     console.error(err);
                                                 return;
                                             } 
-                                            //interaction.reply(`Added ${mapsToAdd5} to the map list`);
+                                            //interaction.reply(`Added ${mapsToAdd4} to the map list`);
                                         });
-                                            if (!mapsToAdd6.length == 0){
-                                                console.log(mapsToAdd6)
-                                                maps.push(mapsToAdd6);
+                                            if (!mapsToAdd5.length == 0){
+                                                console.log(mapsToAdd5)
+                                                maps.push(mapsToAdd5);
                                                 
-                                                fs.appendFile(filePath, mapsToAdd6 + '\n', (err, data) => {
+                                                fs.appendFile(filePath, mapsToAdd5 + '\n', (err, data) => {
                                                     if (err) {
                                                         console.error(err);
                                                     return;
                                                 } 
-                                                //interaction.reply(`Added ${mapsToAdd6} to the map list`);
+                                                //interaction.reply(`Added ${mapsToAdd5} to the map list`);
                                             });
-                                                if (!mapsToAdd7.length == 0){
-                                                    console.log(mapsToAdd7)
-                                                    maps.push(mapsToAdd7);
+                                                if (!mapsToAdd6.length == 0){
+                                                    console.log(mapsToAdd6)
+                                                    maps.push(mapsToAdd6);
                                                     
-                                                    fs.appendFile(filePath, mapsToAdd7 + '\n', (err, data) => {
+                                                    fs.appendFile(filePath, mapsToAdd6 + '\n', (err, data) => {
                                                         if (err) {
                                                             console.error(err);
                                                         return;
                                                     } 
-                                                    //interaction.reply(`Added ${mapInt} to the map list`);
+                                                    //interaction.reply(`Added ${mapsToAdd6} to the map list`);
                                                 });
-                                                } 
+                                                    if (!mapsToAdd7.length == 0){
+                                                        console.log(mapsToAdd7)
+                                                        maps.push(mapsToAdd7);
+                                                        
+                                                        fs.appendFile(filePath, mapsToAdd7 + '\n', (err, data) => {
+                                                            if (err) {
+                                                                console.error(err);
+                                                            return;
+                                                        } 
+                                                        //interaction.reply(`Added ${mapInt} to the map list`);
+                                                    });
+                                                    } 
+                                                }
                                             }
                                         }
                                     }
                                 }
-                            }
-                        }     
+                            }     
+                        }
                     } else {
-                        interaction.reply({content : `${mapInt} is mispelled or not a map`, ephemeral: true });
+                        interaction.reply({content : `${mapInt} is already on the map list`, ephemeral: true });
                     }
                 }              
             } else if (commandInt === 'roulette' || commandInt === 'r') {
