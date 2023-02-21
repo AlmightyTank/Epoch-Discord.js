597311b3c5fe72279c99d76e169aef233e75cbfc
Christopher Young <chrisyoungplus@outlook.com>
2023-02-21 02:43:35 +0000

    Updating Streamer List

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -6,32 +6,37 @@ const twitchAPIEndpoint = 'https://api.twitch.tv';
 const streamers = [
     {
       name: 'streamer1',
-      twitch_username: 'BakedBackHouse',
+      twitch_username: 'bakedbackhouse',
       live: false,
     },
     {
       name: 'streamer2',
-      twitch_username: 'ToastRackTV',
+      twitch_username: 'toastracktv',
       live: false,
     },
     {
       name: 'streamer3',
-      twitch_username: 'AlmightyTank',
+      twitch_username: 'almightytank',
       live: false,
     },
     {
       name: 'streamer4',
-      twitch_username: 'VeryBadScav',
+      twitch_username: 'verybadscav',
       live: false,
     },
     {
       name: 'streamer5',
-      twitch_username: 'DuskyReaper07',
+      twitch_username: 'duskyreaper07',
       live: false,
     },
     {
       name: 'streamer6',
-      twitch_username: 'AirWingMarine',
+      twitch_username: 'airwingmarine',
+      live: false,
+    },
+    {
+      name: 'streamer7',
+      twitch_username: 'jessekazam',
       live: false,
     },
   ];
@@ -87,22 +92,38 @@ module.exports = {
         }
 
         // List of Activity that are picked at random when no one is live
+        //Playing = 0, Streaming = 1, Listening to = 2, Watching = 3, Competing = 5
         const list = [
           { name: 'AlMightyTank#6286', number: 3 },
           { name: 'Myself get Lost', number: 3 },
           { name: 'Word of Wisdom', number: 2 },
           { name: 'with Tornados', number: 0 },
+          { name: 'the Screams from Below', number: 2 },
+          { name: 'the Fight for Life in Tarkov', number: 5 },
         ];
-        
-        // Define a function that picks a random element from the list, along with its number
-        function pickRandomFromList(list) {
-          const randomIndex = Math.floor(Math.random() * list.length);
-          const randomElement = list[randomIndex];
+
+        // Define an array to store the previous picks
+        const previousPicks = [];
+
+        // Define a function that picks a random element from the list, along with its number, without repeating previous picks
+        function pickRandomFromList(list, previousPicks) {
+          let randomElement = null;
+          while (!randomElement || previousPicks.includes(randomElement.name)) {
+            const randomIndex = Math.floor(Math.random() * list.length);
+            randomElement = list[randomIndex];
+          }
           return { name: randomElement.name, number: randomElement.number };
         }
+        
+        // Define a function that picks a random element from the list, along with its number
+        //function pickRandomFromList(list) {
+        //  const randomIndex = Math.floor(Math.random() * list.length);
+        //  const randomElement = list[randomIndex];
+        //  return { name: randomElement.name, number: randomElement.number };
+        //}
          
         function updateStatus(streamers) {
-            const url = `${twitchAPIEndpoint}/helix/streams?user_login=${streamers.toLowerCase()}`;
+            const url = `${twitchAPIEndpoint}/helix/streams?user_login=${streamers}`;
             const options = {
               method: 'GET',
               headers: {
@@ -125,19 +146,26 @@ module.exports = {
                     const streamData = data.data[0];
                     const streamTitle = streamData.title;
                     const streamGame = streamData.game_name;
-                    const streamURL = `https://www.twitch.tv/${streamers.toLowerCase()}`;
+                    const streamerName = streamData.user_name;
+                    const streamURL = `https://www.twitch.tv/${streamers}`;
                     //client.user.setActivity(`${streamers} is live!`, { type: "STREAMING", url: streamURL });
-                    client.user.setActivity(`${streamers} playing ${streamGame}!`, {
+                    client.user.setActivity(`${streamerName} playing ${streamGame}!`, {
                         type: "STREAMING",
                         url: streamURL
                     });
                     client.user.setStatus("online");
                     console.log(`[LOG] ${streamers} is live`)
                 } else {
-                    const randomElement = pickRandomFromList(list);
+                    //const randomElement = pickRandomFromList(list);
+                    const randomElement = pickRandomFromList(list, previousPicks);
+                    previousPicks.push(randomElement.name);
                     client.user.setActivity(`${randomElement.name}`, { type: randomElement.number});
                     client.user.setStatus("online");
-                    console.log(`[LOG] ${streamers} is not live and picked ${randomElement.name}`)
+                    console.log(`[LOG] ${streamers} is not live and picked ${randomElement.name} and ${previousPicks.length}`);
+                    if(previousPicks.length > 4) {
+                      previousPicks.length = 0;
+                      console.log(`[LOG] Previous Picks CLEARED - ${previousPicks.length}`);
+                    }
                 }
               })
               .catch(error => {
@@ -154,6 +182,6 @@ module.exports = {
                     updateStatus(streamers.twitch_username);
                 }, 60000 * i);
             });
-        }, 420000); 
+        }, 60000 * (streamers.length + 1)); 
     }
 }
